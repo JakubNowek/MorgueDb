f788f0a2a0b7b4101594b8a1588ec3ef95cf3a9d
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-05-25 21:40:04 +0200

    Reimplemented patient view

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 import tkinter.messagebox
 import customtkinter
 import db_connection
@@ -69,7 +69,7 @@ class App(customtkinter.CTk):
         self.button_1 = customtkinter.CTkButton(master=self.frame_left,
                                                 text="Pacjenci",
                                                 fg_color=("gray75", "gray30"),  # <- custom tuple-color
-                                                command=self.button_pacjenci)
+                                                command=self.button_pacjenci_another_way)
         self.button_1.grid(row=2, column=0, pady=10, padx=20)
 
         self.button_2 = customtkinter.CTkButton(master=self.frame_left,
@@ -81,7 +81,7 @@ class App(customtkinter.CTk):
         self.button_3 = customtkinter.CTkButton(master=self.frame_left,
                                                 text="Grafik",
                                                 fg_color=("gray75", "gray30"),  # <- custom tuple-color
-                                                command=self.button_event)
+                                                command=self.button_pacjenci_another_way)
         self.button_3.grid(row=4, column=0, pady=10, padx=20)
 
         self.switch_1 = customtkinter.CTkSwitch(master=self.frame_left)
@@ -101,15 +101,9 @@ class App(customtkinter.CTk):
         self.frame_info.tkraise()
         # ============ frame_pacjenci ============
 
-        self.frame_pacjenci.rowconfigure(0, weight=1)
-        self.frame_pacjenci.columnconfigure(0, weight=1)
 
-        self.label_info_2 = customtkinter.CTkLabel(master=self.frame_pacjenci,
-                                                   text="Dupa",
-                                                   height=100,
-                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
-                                                   justify=tkinter.LEFT)
-        self.label_info_2.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+
+
         # self.button4 = customtkinter.CTkButton(master=self.frame_pacjenci,
         #                                                 text="dupa",
         #                                                 fg_color=("gray75", "gray30"),  # <- custom tuple-color
@@ -120,36 +114,66 @@ class App(customtkinter.CTk):
     def button_event(self):
         print("Button pressed")
 
-    def button_pacjenci(self):
-        self.frame_info.tkraise()
+    # def button_pacjenci(self):
+    #     self.frame_info.tkraise()
+    #     x = db_connection.moreguDB()
+    #     df = x.test_connection()
+    #     pt = Table(self.frame_info,dataframe=df,showtoolbar=True, showstatusbar=True)
+    #     options = {'align': 'w',
+    #          'cellbackgr': '#F4F4F3',
+    #          'cellwidth': 80,
+    #          'floatprecision': 2,
+    #          'thousandseparator': '',
+    #          'font': 'Arial',
+    #          'fontsize': 12,
+    #          'fontstyle': '',
+    #          'grid_color': '#ABB1AD',
+    #          'linewidth': 1,
+    #          'rowheight': 22,
+    #          'rowselectedcolor': '#E4DED4',
+    #          'textcolor': 'black'}
+    #     config.apply_options(options, pt)
+    #
+    #     pt.show()
+
+    def button_pacjenci_another_way(self):
         x = db_connection.moreguDB()
-        df = x.test_connection()
-        pt = Table(self.frame_info,dataframe=df,showtoolbar=True, showstatusbar=True)
-        options = {'align': 'w',
-             'cellbackgr': '#F4F4F3',
-             'cellwidth': 80,
-             'floatprecision': 2,
-             'thousandseparator': '',
-             'font': 'Arial',
-             'fontsize': 12,
-             'fontstyle': '',
-             'grid_color': '#ABB1AD',
-             'linewidth': 1,
-             'rowheight': 22,
-             'rowselectedcolor': '#E4DED4',
-             'textcolor': 'black'}
-        config.apply_options(options, pt)
+        df = x.test_connection().values.tolist()
+        self.frame_pacjenci.tkraise()
+        self.frame_pacjenci.grid_columnconfigure(1, weight=1)
+        tk.Label(master=self.frame_pacjenci, text="Id lekarza", anchor="w").grid(row=0, column=0, sticky="ew")
+        tk.Label(master=self.frame_pacjenci, text="Id pracownika", anchor="w").grid(row=0, column=1, sticky="ew")
+        tk.Label(master=self.frame_pacjenci, text="Imie", anchor="w").grid(row=0, column=2, sticky="ew")
+        tk.Label(master=self.frame_pacjenci, text="Nazwisko", anchor="w").grid(row=0, column=3, sticky="ew")
+        tk.Label(master=self.frame_pacjenci, text="Telefon", anchor="w").grid(row=0, column=4, sticky="ew")
+        tk.Label(master=self.frame_pacjenci, text="Godziny pracy", anchor="w").grid(row=0, column=5, sticky="ew")
+        tk.Label(master=self.frame_pacjenci, text="Szczegóły", anchor="w").grid(row=0, column=6, sticky="ew")
+        row=1
 
-        pt.show()
+        for (id_lekarza, id_pracownika, imie,nazwisko,telefon,godziny_pracy) in df:
+            id_lekarza_label = tk.Label(master=self.frame_pacjenci, text=str(id_lekarza), anchor="w")
+            id_pracownika_label = tk.Label(master=self.frame_pacjenci, text=str(id_lekarza), anchor="w")
+            imie_label = tk.Label(master=self.frame_pacjenci, text=imie, anchor="w")
+            nazwisko_label = tk.Label(master=self.frame_pacjenci, text=nazwisko, anchor="w")
+            telefon_label = tk.Label(master=self.frame_pacjenci, text=telefon, anchor="w")
+            godziny_pracy_label = tk.Label(master=self.frame_pacjenci, text=godziny_pracy, anchor="w")
+            action_button = tk.Button(master=self.frame_pacjenci, text="Szczegóły",)
 
+            id_lekarza_label.grid(row=row, column=0, sticky="ew")
+            id_pracownika_label.grid(row=row, column=1, sticky="ew")
+            imie_label.grid(row=row, column=2, sticky="ew")
+            nazwisko_label.grid(row=row, column=3, sticky="ew")
+            telefon_label.grid(row=row, column=4, sticky="ew")
+            godziny_pracy_label.grid(row=row, column=5, sticky="ew")
+            action_button.grid(row=row, column=6, sticky="ew")
 
-
+            row += 1
 
     def change_mode(self):
-        if self.switch_2.get() == 1:
-            customtkinter.set_appearance_mode("dark")
-        else:
-            customtkinter.set_appearance_mode("light")
+            if self.switch_2.get() == 1:
+                customtkinter.set_appearance_mode("dark")
+            else:
+                customtkinter.set_appearance_mode("light")
 
     def on_closing(self, event=0):
         self.destroy()
