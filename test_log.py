53da5c58555c78c05b8fd3c15e2b30ca081bd4d0
JakubNowek <jakubbnwek@gmail.com>
2022-06-06 11:45:06 +0200

    buwno

--- test_log.py
+++ test_log.py
@@ -15,14 +15,15 @@ class Login_Screen(customtkinter.CTk):
     WIDTH = 840
     HEIGHT = 520
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
 
         self.title("MorgueDB")
         self.geometry(f"{Login_Screen.WIDTH}x{Login_Screen.HEIGHT}")
-        self.minsize(Login_Screen.WIDTH, Login_Screen.HEIGHT)
-        self.maxsize(Login_Screen.WIDTH, Login_Screen.HEIGHT)
-        self.resizable(False, False)
+        # jak się to odkomentuje, to nie będzie można rozciągać okienka
+        # self.minsize(Login_Screen.WIDTH, Login_Screen.HEIGHT)
+        # self.maxsize(Login_Screen.WIDTH, Login_Screen.HEIGHT)
+        # self.resizable(False, False)
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
@@ -31,16 +32,19 @@ class Login_Screen(customtkinter.CTk):
         self.bg_image = ImageTk.PhotoImage(image)
 
         self.image_label = tkinter.Label(master=self, image=self.bg_image)
-        self.image_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.image_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+
 
         self.label_1 = customtkinter.CTkLabel(master=self, width=200, height=60,
                                               fg_color=("gray70", "gray25"), text="CustomTkinter\ninterface example")
         self.label_1.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-        self.entry_1 = customtkinter.CTkEntry(master=self, corner_radius=6, width=200, placeholder_text="username")
+        self.entry_1 = customtkinter.CTkEntry(master=self, corner_radius=6, width=200, placeholder_text="nazwa użytkownika")
         self.entry_1.place(relx=0.5, rely=0.52, anchor=tkinter.CENTER)
 
-        self.entry_2 = customtkinter.CTkEntry(master=self, corner_radius=6, width=200, show="*", placeholder_text="password")
+        self.entry_2 = customtkinter.CTkEntry(master=self, corner_radius=6, width=200, show="*", placeholder_text="hasło")
         self.entry_2.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
         self.button_2 = customtkinter.CTkButton(master=self, text="Login",
